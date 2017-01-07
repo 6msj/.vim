e494c557e65dabccf0864c6e758e936ceceebc7b
James Nguyen <ja.nguyen@gmail.com>
2017-01-06 19:16:26 -0800

    Tweak

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "updating git submodules"
-git submodule update --init --recursive
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
+vim +PlugInstall +qall
